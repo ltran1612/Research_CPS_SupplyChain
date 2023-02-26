61d18c3f5e711f7045abdedbb5af9d8f8c69ccc1
ltran <longdtranw@gmail.com>
2023-02-26 16:27:26 -0700

    add TEST usercase to learn more about the program

--- src/asklab/querypicker/QUERIES/TEST/generate.sh
+++ src/asklab/querypicker/QUERIES/TEST/generate.sh
@@ -7,19 +7,20 @@ REASONING=../Sophisticated-Reasoning/
 # Build files OWL Ontoly Case 1
 # Most/Least Trustworthy Component (Case 0)
 FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_driven.lp $REASONING/Integration/SR-RE-01-truthworthiness/ontology_init_ele_0.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_1_onto.lp output_sr_re_01_01.lp"
-OUTPUT=FULL-SR-ER-01-ELE-01-00.txt
+OUTPUT=TEST-01.txt
 cat $FILES > $OUTPUT
 
 # Check unsatisfied concerns (Case 0) 
 FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_driven.lp $REASONING/Integration/SR-RE-01-truthworthiness/ontology_init_ele_0.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_2_onto.lp output_sr_re_01_02.lp"
-OUTPUT=FULL-SR-ER-01-ELE-02-00.txt
+OUTPUT=TEST-02.txt
 cat $FILES > $OUTPUT
 
 # Mitigation Strategy: Random method + List all possible solutions (Case 0)
 FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_driven.lp $REASONING/Integration/SR-RE-01-truthworthiness/ontology_init_ele_0.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_3_onto.lp output_sr_re_01_03.lp"
-OUTPUT=FULL-SR-ER-01-ELE-03-00.txt
+OUTPUT=TEST-03.txt
 cat $FILES > $OUTPUT
 
+# Mitigation Strategy: Highest chance to succeed (Probabilistic Reasoning) (Case 0)
 FILES="$REASONING/Integration/SR-RE-01-truthworthiness/ontology_driven.lp $REASONING/Integration/SR-RE-01-truthworthiness/ontology_init_ele_0.lp $REASONING/Integration/SR-RE-01-truthworthiness/reasoning_2_4_onto.lp output_sr_re_01_04.lp"
-OUTPUT=FULL-SR-ER-01-ELE-04-00.txt
+OUTPUT=TEST-04.txt
 cat $FILES > $OUTPUT
