ea2a2dfa6fef64e223d5f2663364bacaeb196c20
ltran <longdtranw@gmail.com>
2023-03-10 13:36:49 -0700

    add the owl file

---
+++ run.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+ASP_UPDATE_FOLDER=/Users/longtran/Personal/CPS/Research_CPS_SupplyChain/src/asklab/querypicker/QUERIES/CONTRACT
+BASE=/Users/longtran/Personal/CPS/Research_CPS_SupplyChain
+cd $ASP_UPDATE_FOLDER
+sh generate.sh
+cd $BASE
+ant build
+ant QueryPicker
