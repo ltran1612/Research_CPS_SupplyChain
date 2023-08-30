4460c1e3e4b8f5f195018b47c70059a762697a17
Long Tran <longdtranw@gmail.com>
2023-08-30 11:20:58 -0600

    started getting test case

---
+++ linux_chmod_auto.sh
@@ -0,0 +1,12 @@
+cd bin/asklab/cpsf
+chmod u+x *.sh
+cd clingo-4.4.0
+chmod u+x clingo-linux-x86
+cd ../dlv
+chmod u+x dlv
+cd ../mkatoms
+chmod u+x mkatoms-linux-x86
+cd ../apache-jena-3.0.0/bin
+chmod u+x *
+
+
--- server_src/test.sh
+++ server_src/test.sh
@@ -5,4 +5,4 @@ curl -X POST \
 -F file2=@"../src/asklab/querypicker/QUERIES/EX4-sr-elevator/cpsframework-v3-sr-Elevator-Configuration.owl" \
 -F file3=@"../src/asklab/querypicker/QUERIES/EX4-sr-elevator/cpsframework-v3-sr-Elevator-Initial.owl" \
 -F file4=@"../src/asklab/querypicker/QUERIES/EX4-sr-elevator/FULL-SR-ER-01-ELE-01-00.txt" \
-http://127.0.0.1:3000/reasoning -o result.txt
+http://127.0.0.1:9000/reasoning -o result.txt
