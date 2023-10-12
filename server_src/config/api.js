5466b8cde7bab3447fa60b57aed4841df3377be4
Long Tran <longdtranw@gmail.com>
2023-10-12 05:38:45 -0600

    updated test case to include picture and to adapt to the new API where files ending with .lp are ASP files, we do not need to specify the names individually

--- server_src/config/api.js
+++ server_src/config/api.js
@@ -17,49 +17,94 @@ module.exports = function (app) {
   app.route("/reasoning").post(reasoning).get(runExample);
 }; // end function
 
+async function combineASPFiles(ASPFiles, file) { 
+  // create the file with the temporary directory 
+  const destPath = file.name;
+
+  // read each lp files
+  // append them to the 
+  for (let fileIdx in ASPFiles) {
+    // get the file object
+    const file = ASPFiles[fileIdx];
+    // get the src of the asp file
+    const srcPath = file.filepath;
+    // read the file
+    const fileContent = await fsp.readFile(srcPath, {encoding: 'utf8'});
+    // append it to the final output
+    await fsp.writeFile(destPath, fileContent, {flag: "a"}); 
+  } // end for
+} // end CombineASPFiles 
+
 async function reasoning(req, res, next) {
   //https://everything.curl.dev/http/multipart
   const form = formidable({ multiples: true });
 
-  let ASPFileName = "";
   let solver = -1;
   form.parse(req, async (err, fields, files) => {
+    // if there is an error
     if (err) {
       next(err);
       return;
     } // end if
 
-    let ASPFileName = fields.aspName;
+    // get the solver
     let solver = fields.solver;
 
+    // create a temporary directory
     const tmpDir = tmp.dirSync({ mode: 0o766 });
-
+    
+    const finalFilesToUse = [];
+    const aspFiles = [];
+    for (let fileIdx in files) {
+      // get the file
+      const myFile = files[fileIdx].toJSON();
+      const name = myFile.originalFilename;
+      if (name.endsWith(".lp")) {
+        aspFiles.push(myFile);
+        continue;
+      } // end if
+      finalFilesToUse.push(myFile);
+    } // end for 
+   
     // copy the ontology from the base folder to here.
     // copyOWL(BASE_CORE_DIR, tmpDir);
     // copyOWL(exampleDir, tmpDir);
-    let fileList = [];
-    for (let file in files) {
-      file = files[file].toJSON();
+    let fileListPromises = [];
+
+    // get the temporary directory path 
+    const tmpDirPath = tmpDir.name;
+
+    // combine files into asp
+    const aspFile = tmp.fileSync({postfix: ".lp"});
+    await combineASPFiles(aspFiles, aspFile);
+    // copy the asp file to the temporary folder
+    let srcPath = aspFile.name;
+    let dstPath = path.join(tmpDirPath, "final.lp");
+    fileListPromises.push(fsp.copyFile(srcPath, dstPath));
+    const ASPFileName = 'final.lp';
+
+    // copy the other files into the temporary folder
+    for (let fileIdx in finalFilesToUse) {
+      const myFile = finalFilesToUse[fileIdx];
       // src path
-      let srcPath = file.filepath;
+      let srcPath = myFile.filepath;
 
       // dst path
-      let dstPath = tmpDir.name;
-      let dstName = file.originalFilename;
-      dstPath = path.join(dstPath, dstName);
+      let dstName = myFile.originalFilename;
+      let dstPath = path.join(tmpDirPath, dstName);
 
       // copy file store the promise
-      fileList.push(fsp.copyFile(srcPath, dstPath));
+      fileListPromises.push(fsp.copyFile(srcPath, dstPath));
     } // end for
 
     // waiting for file copy to be done
     try {
-      await Promise.all(fileList);
+      await Promise.all(fileListPromises);
     } catch (e) {
       console.error(e);
     } // end catch
 
-    //files = await fsp.readdir(tmpDir.name);
+    // files = await fsp.readdir(tmpDir.name);
 
     // await query
     let result = await query(tmpDir, ASPFileName, solver);
