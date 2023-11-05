f059addcdda04fbc68a024b6ea0b67c19064d8fa
Long Tran <longdtranw@gmail.com>
2023-11-05 16:05:31 -0700

    add logging and set logging

--- agent_environment/env.py
+++ agent_environment/env.py
@@ -3,15 +3,20 @@ from misc import load_config, show_config
 from threading import Lock, Thread 
 from time import sleep
 import subprocess
+import logging
+import sys
 
+# see the config
 config = load_config()
 show_config(config)
 
+# parse the config
 pairs = config['pairs']
 agents = list(pairs.keys())
 messages = {}
 messageLock = Lock() 
 
+# initialized the received queue  
 received = [] 
 step = 0
 receivedLock = Lock()
@@ -19,7 +24,7 @@ receivedLock = Lock()
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client: mqtt.Client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    logging.debug("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe('env/+')
@@ -32,12 +37,12 @@ def on_message(client: mqtt.Client, userdata, msg):
 
     for agent in agents:
         if topic == f"env/{agent}":
-            print(f"received from {agent}")
+            logging.info(f"received from {agent}")
             messageLock.acquire()
             messages[agent] = message
             messageLock.release()
         elif topic == f"recv/{agent}":
-            print(f"{agent} received")
+            logging.info(f"{agent} received")
             receivedLock.acquire()
             received.append(agent) 
             receivedLock.release()
@@ -70,16 +75,21 @@ def custom_loop():
                     name = f"{target_name}_temp.lp" 
                     with open(name, "w") as f:
                         f.write(message)
-                    command = ["clingo", target_parser, name] 
+                    command = ["clingo", target_parser, name, "-V0", "--out-atom=%s."] 
                     result= subprocess.run(command, capture_output=True)
                     parsed_message = result.stdout.decode("utf-8")
+
+                    logging.info("hello")
+                    if "UNSATISFIABLE" in parsed_message:
+                        pass
+                        
 
                     # add to the final message
                     final_message.append(parsed_message)
 
                 final_message = "\n".join(final_message)
-                print("send state information")
                 client.publish(f"for/{agent}", final_message, qos=2)
+                logging.info(f"sent state information to {agent}")
             # reset the messages storage
             messages = {}
         messageLock.release()
@@ -89,7 +99,7 @@ def custom_loop():
         receivedLock.acquire()
         if len(received) == len(agents):
             step += 1 
-            print("received all, start the next step")
+            logging.info(f"received all, start the next step {step}")
             for agent in agents:
                 client.publish(f"next/{agent}", str(step), qos=2, retain=False)
 
@@ -98,6 +108,14 @@ def custom_loop():
 
         receivedLock.release()
         sleep(1)
+
+# set the logging
+log_handler = logging.StreamHandler(sys.stdout)
+log_handler.setLevel(logging.INFO)
+log_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().addHandler(log_handler)
+
 
 # run the thread
 my_thread = Thread(target=custom_loop, args=())
