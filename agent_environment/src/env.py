45220f5c019769be000ff317abe85bfbf8afaa62
Long Tran <longdtranw@gmail.com>
2023-11-27 17:10:05 -0700

    update environment to move message buffer, and received checker to become modular

--- agent_environment/src/env.py
+++ agent_environment/src/env.py
@@ -1,10 +1,11 @@
 import paho.mqtt.client as mqtt
-from misc import load_config, show_config, run_clingo, write_to_temp_file
-from threading import Lock, Thread 
+from misc import load_config, show_config 
+from threading import Thread 
 from time import sleep
 import logging
 import sys
-from env_misc import StateManger
+from env_misc import StateManger, Received
+from queue import Queue
 
 # load and display the config
 config = load_config()
@@ -13,19 +14,14 @@ show_config(config)
 pairs = config['pairs']
 agents = list(pairs.keys())
 
-# messages storage
-messages = {}
-messageLock = Lock() 
-
 # initialized the received queue  
-received = [] 
-receivedLock = Lock()
+received = Received(agents) 
 
 # step
 step = -1
 
 # set up
-states = StateManger(agents)
+state = StateManger(agents)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client: mqtt.Client, userdata, flags, rc):
@@ -43,14 +39,10 @@ def on_message(client: mqtt.Client, userdata, msg):
     for agent in agents:
         if topic == f"env/{agent}":
             logging.info(f"received from {agent}")
-            messageLock.acquire()
-            messages[agent] = message
-            messageLock.release()
+            state.receive_message(agent, message)
         elif topic == f"recv/{agent}":
             logging.info(f"{agent} received")
-            receivedLock.acquire()
-            received.append(agent) 
-            receivedLock.release()
+            received.receive(agent)
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -61,63 +53,24 @@ client.on_message = on_message
 # custom loop function that will loop forever
 def custom_loop(): 
     while True:
-        global received
         global step
-        global messages
 
         # we received fully everything
         # compile information
         # send the information to each agent
-        messageLock.acquire()
-        if len(messages.keys()) == len(agents):
+        if state.received_all():
+            # calcualte the global next state
+            state.calculate_state()
+
+            # then, for each agent, pick out the important information. 
             for agent in agents:
-                # intialize the message to contain its current messages
-                final_message = [messages[agent]]
-
                 # take the message from the agent that concerns this agent.
-                # then add them to a final message
-                for target in pairs[agent]:
-                    # get the concerned agent's name
-                    target_name = target["name"]
-                    # get the right file to parse that agent's message
-                    target_parser = target["parser"]
-                    # get the message to parse
-                    message = messages[target_name]
-
-                    # parse the message 
-                    # put the message to a temporary file
-                    message_file = f"env_{target_name}_temp.lp" 
-                    write_to_temp_file(message_file, message)
-
-                    # run clingo with the message and the parser file
-                    hasFoundAnswerSet, answerSet = run_clingo([target_parser, message_file])
-
-                    # check for potential error, if not clean the message
-                    if not hasFoundAnswerSet:
-                        logging.error("Parsing is unsatisfiable somehow")
-                        exit(1)
-                    else:
-                        logging.debug(f"parsed these atoms array {answerSet}")
-                        # add to the final message
-                        final_message.extend(answerSet)
-                
-                # compose the final message from the messsage of all agents 
-                final_message = " ".join(final_message)
-                logging.debug(f"the message from the parsing is {final_message}")
-                # add the final message to the state of a agent
-                states.add_to_state(agent, final_message)
-                # send the state information to the agent
-                client.publish(f"for/{agent}", states.get_state(agent), qos=2)
+                client.publish(f"for/{agent}", state.get_state(agent), qos=2)
                 logging.info(f"sent state information to {agent}")
-
-            # reset the messages storage
-            messages = {}
-        messageLock.release()
 
         # all users have received the information we sent
         # sent them the message to do the next step
-        receivedLock.acquire()
-        if len(received) == len(agents):
+        if received.received_all(): 
             # environment control the signal for the next step
             # increase the step
             step += 1 
@@ -128,8 +81,7 @@ def custom_loop():
                 client.publish(f"next/{agent}", str(step), qos=2, retain=False)
 
             # reset received array
-            received = []            
-        receivedLock.release()
+            received.reset()
 
         # sleep for 1 second
         sleep(1)
--- agent_environment/src/env_misc.py
+++ agent_environment/src/env_misc.py
@@ -1,12 +1,105 @@
-# manage the information state of each agent
+from threading import Lock 
+
+# manage the global state information 
+# thread-safe
 class StateManger:
-    states = {}
+    state = "" 
+    states = {} 
+    messages = {}
+    setup_info = {}
+    agents = []
+
+    lock = Lock() 
     def __init__(self, agents):
+        self.agents = agents
         for agent in agents:
             self.states[agent] = ""
-    
+
+    # function to receive setup information from the domain
+    def setup(self, agent, setup_info):    
+        self.lock.acquire()
+        self.lock.release()
+
+    # function to receive the message 
+    def receive_message(self, agent, message):
+        self.lock.acquire()
+        self.messages[agent] = message
+        self.lock.release()
+
+    # function to check if all agent sent the messages
+    def received_all(self) -> bool:
+        result: bool = False
+
+        self.lock.acquire()
+        result = len(self.messages) == len(self.agents)
+        self.lock.release()
+
+        return result
+
+    # calculate state
+    def calculate_state(self): 
+        self.lock.acquire()
+        # calculate states - TODO
+        self.state = ""
+        for agent in self.agents:
+            self.states[agent] = ""
+
+        # reset message buffer
+        self.messages = {}
+        self.lock.release()
+
+    # check if setup
+    def is_setup(self) -> bool:
+        result: bool = False
+
+        self.lock.acquire()
+        result = len(self.setup_info) == len(self.agents)
+        self.lock.release()
+
+        return result
+
+    # function to get the state relates to an agent 
     def get_state(self, agent):
-        return self.states[agent]
+        state = None
+
+        self.lock.acquire()
+        state = self.states[agent]
+        self.lock.release()
+
+        return state
+
+class Received:
+    received = {}
+    receiveLock = Lock()
+
+    def __init__(self, agents):
+        self.agents = agents
     
-    def add_to_state(self, agent, message):
-        self.states[agent] += " " + message 
+    def received_all(self) -> bool:
+        result: bool = False
+
+        self.receiveLock.acquire()
+        result = len(self.received.keys()) == len(self.agents) 
+        self.receiveLock.release()
+
+        return result
+        
+    def receive(self, agent):
+        self.receiveLock.acquire()
+        self.received[agent] = None
+        self.receiveLock.release()
+
+    def reset(self):
+        self.receiveLock.acquire()
+        self.received = {}
+        self.receiveLock.release()
+
+    def is_in(self, agent):
+        result: bool = False
+
+        self.receiveLock.acquire()
+        result = agent in self.received.keys()
+        self.receiveLock.release()
+
+        return result
+
