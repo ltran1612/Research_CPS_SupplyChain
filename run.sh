1216df8bce5c554d1be2de73a1bb90eaab91cb80
Long Tran <longdtranw@gmail.com>
2023-12-25 16:41:17 +0700

    resolve conflict when merging one-domain branch to master

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-ASP_UPDATE_FOLDER=/Users/longtran/Personal/CPS/Research_CPS_SupplyChain/src/asklab/querypicker/QUERIES/CONTRACT
+ASP_UPDATE_FOLDER=/Users/longtran/Personal/CPS/Research_CPS_SupplyChain/src/asklab/querypicker/QUERIES/TEST
 BASE=/Users/longtran/Personal/CPS/Research_CPS_SupplyChain
 cd $ASP_UPDATE_FOLDER
 sh generate.sh
