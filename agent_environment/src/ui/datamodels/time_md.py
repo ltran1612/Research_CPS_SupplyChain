a3f9beec74ef0ef981359164d30c981ff5cedd0f
Long Tran <longdtranw@gmail.com>
2024-08-06 02:57:33 -0700

    update the time ui when time changes

--- agent_environment/src/ui/datamodels/time_md.py
+++ agent_environment/src/ui/datamodels/time_md.py
@@ -8,18 +8,36 @@ class TimeModel(DataModel):
         super().__init__()
         self.time = start 
         self.start = start
+        self.max = start
+        self.timeLabel = None
 
     # load the data from a string
     def load_from_string(self, s):
-        self.time = int(s)
+        t = int(s)
+        if t == self.max:
+            return
+            
+        self.time = t
+        self.max = self.time
+        self._update_time() 
+
+    def _update_time(self):
+        if self.timeLabel is None:
+            return
+        # update the label 
+        self.timeLabel.config(text=self)
+    
+    def __str__(self) -> str:
+        return f"{self.time}:{self.max}"
 
     # fill function, to be implemented by child classes
     def fill(self, frame):
-        timeLabel = tk.Label(frame, text="0:0")
-        timeLabel.pack(side="left", padx=5, pady=5)
+        self.timeLabel = tk.Label(frame, text=self)
+        self.timeLabel.pack(side="left", padx=5, pady=5)
 
         nextButton= tk.Button(frame, text="Next")
         nextButton.pack(side="left", padx=5, pady=5)
 
         runButton = tk.Button(frame, text="Run")
         runButton.pack(side="left", padx=5, pady=5)
+    
