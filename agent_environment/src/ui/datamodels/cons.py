4b49667e703259275852d3a88acb0b98f2798176
Long Tran <longdtranw@gmail.com>
2024-08-30 10:08:46 -0600

    display the concerns in a clear ui representation

--- agent_environment/src/ui/datamodels/cons.py
+++ agent_environment/src/ui/datamodels/cons.py
@@ -1,16 +1,22 @@
 import logging
+from tkinter import ttk
 from ui.datamodels.base import DataModel
 import tkinter as tk
+
+from ui.widgets.checkbox_viewtable import CheckBoxViewTable
+from ui.widgets.scrollframe import ScrollableFrame
+from ui.widgets.scrolltext import TextboxWithScrollbars
 
 class ConcernModel(DataModel):
     def __init__(self) -> None:
         super().__init__()
         self.data = {
             "clause": {},
+            # also called, requirement
             "property": {},
             "concern": {},
         }
-        self.label = None
+        self.ui_updates = []
 
     def __str__(self):
         def group_str(group: dict[str, bool], title):
@@ -33,7 +39,6 @@ class ConcernModel(DataModel):
         for ctgry, group in self.data.items():
             res.append(group_str(group, ctgry))
         return "\n".join(res)
-
 
     # Abstract
     # load the data from a string
@@ -59,10 +64,60 @@ class ConcernModel(DataModel):
             # add to group
             group[name] = status
         # update
-        if self.label is not None:
-            self.label.config(text=self)
+        for update_func in self.ui_updates:
+            update_func(self.data)
+        
 
     # fill function, to be implemented by child classes
     def fill(self, frame):
-        self.label = tk.Label(frame, text=self.__str__())
-        self.label.pack(pady=20)
+        scanvas = ScrollableFrame(frame) 
+
+        # clauses 
+        clauses = CheckBoxViewTable(scanvas.scrollable_frame)
+
+        # requirements
+        reqs = CheckBoxViewTable(scanvas.scrollable_frame)
+        # concerns
+        concerns = CheckBoxViewTable(scanvas.scrollable_frame)
+
+        # clauses
+        label = tk.Label(scanvas.scrollable_frame, text="Clauses")
+        #
+        label.pack(anchor="nw")
+        clauses.pack(anchor="nw")
+        # requirements
+        label = tk.Label(scanvas.scrollable_frame, text="Requirements")
+        label.pack(anchor="nw")
+        reqs.pack(anchor="nw")
+        # concerns
+        label = tk.Label(scanvas.scrollable_frame, text="Concerns")
+        label.pack(anchor="nw")
+        concerns.pack(anchor="nw")
+
+        # 
+        scanvas.pack(fill="both", expand="True")
+
+        def update_content(data):
+            for ctgry, group in data.items():
+                sat_box = None
+                notsat_box = None
+                box = None
+                # clause
+                if ctgry == "clause":
+                    box = clauses
+                # requirement/property
+                elif ctgry == "property":
+                    box = reqs
+                # concerns
+                elif ctgry == "concern":
+                    box = concerns
+                # unknown type
+                else:
+                    logging.error("In concern model, unknown type of data.")
+                # 
+                if box is not None and len(group) > 0:
+                    box.update_values(group)
+
+        
+        update_content(self.data)
+        self.ui_updates.append(update_content)
